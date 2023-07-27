94f97dbbf39650bc0548d1e555d5d267f640dbdd
kosne1 <akolia2017@list.ru>
2023-07-27 21:31:40 +0300

    fix: remove prints

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ from app.scheduler import scheduler, check_finished_dairy_notification
 if __name__ == '__main__':
     scheduler.add_job(check_finished_dairy_notification, 'interval', minutes=1)
     scheduler.start()
-    print('Bot is running...')
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
