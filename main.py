58352baa6afcddf7303fa356ff7113811d5e7a23
kosne1 <akolia2017@list.ru>
2023-07-29 01:23:08 +0300

    fix: scheduler

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from datetime import datetime
-
 from aiogram import executor
-from apscheduler.triggers.cron import CronTrigger
 
 from app.commands import *
 from app.handlers import *
 from app.scheduler import scheduler, check_finished_dairy_notification
 
 if __name__ == '__main__':
-    scheduler.add_job(func=check_finished_dairy_notification,
-                      trigger=CronTrigger(hour='0,9,12,20'), run_date=datetime(2023, 7, 28, 12, 30, 0))
+    scheduler.add_job(check_finished_dairy_notification, 'cron', hour='0, 9, 12, 20')
     scheduler.start()
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
