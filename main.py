bccdbb29c37f5291c6e9f4bf17d1015d462159ad
kosne1 <akolia2017@list.ru>
2023-08-09 12:35:36 +0300

    refactor: scheduler job hours

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from app.handlers import *
 from app.scheduler import scheduler, check_finished_dairy_notification
 
 if __name__ == '__main__':
-    scheduler.add_job(check_finished_dairy_notification, 'cron', hour='0, 9, 12, 20')
+    scheduler.add_job(check_finished_dairy_notification, 'cron', hour='1, 9, 12, 20')
     scheduler.start()
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
