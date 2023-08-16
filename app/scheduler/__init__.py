0823f057cc47568778ebe1480d8eb1ec17696926
kosne1 <akolia2017@list.ru>
2023-08-16 11:14:36 +0300

    feat: try except

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -9,14 +9,19 @@ from app.services import user_service
 
 
 async def check_finished_dairy_notification() -> None:
+    log_file = open("error.log", "w")
     current_hour = datetime.datetime.today().hour
     users = user_service.get_users()
     for user in users:
-        if current_hour == 1:
-            user = user.is_finished_diary_today = False
-            user_service.upsert_user(user)
-        elif not user.is_finished_diary_today and current_hour in notification_hours:
-            await bot.send_message(chat_id=user.chat_id, text='Пожалуйста, заполните дневник сна!',
-                                   reply_markup=keyboard_start_buttons)
+        try:
+            if current_hour == 1:
+                user = user.is_finished_diary_today = False
+                user_service.upsert_user(user)
+            elif not user.is_finished_diary_today and current_hour in notification_hours:
+                await bot.send_message(chat_id=user.chat_id, text='Пожалуйста, заполните дневник сна!',
+                                       reply_markup=keyboard_start_buttons)
+        except Exception as e:  # most generic exception you can catch
+            log_file.write(f"[ERROR {datetime.datetime.now()}]: {str(user)}, {str(e)}")
+
 
 scheduler = AsyncIOScheduler()
