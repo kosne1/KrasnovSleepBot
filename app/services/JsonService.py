411799c0abb136e4dc46739f1f5c4f08b4d9ee7c
kosne1 <akolia2017@list.ru>
2023-08-18 10:29:26 +0300

    feat: update_is_finished_diary_to_false

--- app/services/JsonService.py
+++ app/services/JsonService.py
@@ -13,3 +13,9 @@ class JsonService:
     def write_users(self, data: list) -> None:
         with open(self.path, 'w') as file:
             json.dump({"users": data}, file)
+
+    def update_is_finished_diary_to_false(self) -> None:
+        data = self.read_users()
+        for user in data:
+            user['is_finished_diary_today'] = False
+        self.write_users(data)
